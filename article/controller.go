02c24dc5abee6ace23aa2b3ea797644e72bf973b
phumaster <pham.ngoc.phu@linecorp.com>
2020-09-16 17:09:09 +0700

    Refactor article controller

--- article/controller.go
+++ article/controller.go
@@ -9,31 +9,33 @@ import (
 // Controller nil
 type Controller struct{}
 
+var service = &Service{}
+
 // GetArticleByID nil
 func (c *Controller) GetArticleByID(ctx echo.Context) error {
 	id := ctx.Param("id")
-	return ctx.String(http.StatusOK, (*Service)(nil).Find(id))
+	return ctx.String(http.StatusOK, service.Find(id))
 }
 
 // CreateArticle nil
 func (c *Controller) CreateArticle(ctx echo.Context) error {
-	return ctx.JSON(http.StatusOK, (*Service)(nil).Create())
+	return ctx.JSON(http.StatusOK, service.Create())
 }
 
 // UpdateArticle nil
 func (c *Controller) UpdateArticle(ctx echo.Context) error {
 	id := ctx.Param("id")
-	return ctx.String(http.StatusOK, (*Service)(nil).Update(id))
+	return ctx.String(http.StatusOK, service.Update(id))
 }
 
 // DeleteArticle nil
 func (c *Controller) DeleteArticle(ctx echo.Context) error {
 	id := ctx.Param("id")
-	return ctx.String(http.StatusOK, (*Service)(nil).Delete(id))
+	return ctx.String(http.StatusOK, service.Delete(id))
 }
 
 // GetArticleList nil
 func (c *Controller) GetArticleList(ctx echo.Context) error {
 	id := ctx.Param("id")
-	return ctx.String(http.StatusOK, (*Service)(nil).All(id))
+	return ctx.String(http.StatusOK, service.All(id))
 }
